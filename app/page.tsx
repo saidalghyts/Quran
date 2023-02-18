c197f9b0416eaac442a8daec2311f50920a592ee
Said Al-Ghiyats <said.ghiyats@gmail.com>
2023-02-18 20:38:35 +0700

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,78 +1,38 @@
 'use client';
-import { useState, useEffect } from 'react';
+import useSWR from 'swr';
 
-type Post = {
-  data: {
-    number: number;
-    sequence: number;
-    numberOfVerses: number;
-    name: {
-      short: string;
-      long: string;
-      transliteration: { en: string; id: string };
-      translation: { en: string; id: string };
-    };
-    revelation: { arab: string; en: string; id: string };
-    tafsir: { id: string };
-    preBismillah: null;
-    verses: {
-      number: { inQuran: number; inSurah: number };
-      meta: {
-        jux: number;
-        page: number;
-        manzil: number;
-        ruku: number;
-        hizbQuarter: number;
-        sajda: { recommended: boolean; obligatory: boolean };
-      };
-      text: {
-        arab: string;
-        transliteration: { en: string };
-      };
-      translation: { en: string; id: string };
-      audio: {
-        primary: string;
-        secondary: [];
-      };
-      tafsir: { id: { short: string; long: string } };
-    }[];
-  };
+interface Data {
+  data: any;
+  number: number;
+}
+
+const url = '/daftar-surah.json';
+const fetcher = async () => {
+  const response = await fetch(url);
+  const data = await response.json();
+  return data;
 };
 
 export default function Home() {
-  const [loading, setLoading] = useState(true);
-  const [posts, setPosts] = useState<Post[]>([]);
-  const items = [1, 2, 3, 4, 5, 6, 7, 8, 10, 11, 12, 13];
+  const { data, error } = useSWR<Data>(url, fetcher);
 
-  useEffect(() => {
-    const fetchPosts = async () => {
-      const res = await fetch('/api/surah');
-      const data = await res.json();
-      setPosts(data);
-      setLoading(false);
-    };
+  if (error) return <div>Error loading data.</div>;
+  if (!data)
+    return (
+      <>
+        {Array.from({ length: 12 }).map((_, index) => (
+          <div key={index} className="items"></div>
+        ))}
+      </>
+    );
 
-    fetchPosts();
-  }, []);
   return (
     <>
-      {loading ? (
-        <main className="mainBar">
-          {items.map((item) => (
-            <div key={item} className="items bg-red-500"></div>
-          ))}
-        </main>
-      ) : (
-        <main className="mainBar">
-          {posts.map((surah: any) => {
-            return (
-              <div key={surah.number} className="items">
-                <div></div>
-              </div>
-            );
-          })}
-        </main>
-      )}
+      <main className="mainBar">
+        {data.data.map((surah: any) => (
+          <div key={surah.number} className="items"></div>
+        ))}
+      </main>
     </>
   );
 }
