f91685f520197860464fbeeeb67c0a08191624cc
Said Al-Ghiyats <said.ghiyats@gmail.com>
2023-02-18 19:21:05 +0700

    update

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,41 @@
 import { useState, useEffect } from 'react';
 
 type Post = {
-  number: number;
+  data: {
+    number: number;
+    sequence: number;
+    numberOfVerses: number;
+    name: {
+      short: string;
+      long: string;
+      transliteration: { en: string; id: string };
+      translation: { en: string; id: string };
+    };
+    revelation: { arab: string; en: string; id: string };
+    tafsir: { id: string };
+    preBismillah: null;
+    verses: {
+      number: { inQuran: number; inSurah: number };
+      meta: {
+        jux: number;
+        page: number;
+        manzil: number;
+        ruku: number;
+        hizbQuarter: number;
+        sajda: { recommended: boolean; obligatory: boolean };
+      };
+      text: {
+        arab: string;
+        transliteration: { en: string };
+      };
+      translation: { en: string; id: string };
+      audio: {
+        primary: string;
+        secondary: [];
+      };
+      tafsir: { id: { short: string; long: string } };
+    }[];
+  };
 };
 
 export default function Home() {
