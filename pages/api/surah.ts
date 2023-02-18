5cda5afff989d4efe1aa925a2401f910cbf25e51
Said Al-Ghiyats <said.ghiyats@gmail.com>
2023-02-18 19:18:59 +0700

    update

--- pages/api/surah.ts
+++ pages/api/surah.ts
@@ -2,9 +2,44 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import postsData from './daftar-surah.json';
 
 type Post = {
-  id: number;
-  title: string;
-  body: string;
+  license: string;
+  source: string;
+  audioEdition: string;
+  data: {
+    number: number;
+    sequence: number;
+    numberOfVerses: number;
+    name: {
+      short: string;
+      long: string;
+      transliteration: { en: string; id: string };
+      translation: { en: string; id: string };
+    };
+    revelation: { arab: string; en: string; id: string };
+    tafsir: { id: string };
+    preBismillah: null;
+    verses: {
+      number: { inQuran: number; inSurah: number };
+      meta: {
+        jux: number;
+        page: number;
+        manzil: number;
+        ruku: number;
+        hizbQuarter: number;
+        sajda: { recommended: boolean; obligatory: boolean };
+      };
+      text: {
+        arab: string;
+        transliteration: { en: string };
+      };
+      translation: { en: string; id: string };
+      audio: {
+        primary: string;
+        secondary: [];
+      };
+      tafsir: { id: { short: string; long: string } };
+    }[];
+  };
 };
 
 type PostsData = {
