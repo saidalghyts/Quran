9225af84170f62a7b0106cc15f142e1fd33a72a6
Said Al-Ghiyats <said.ghiyats@gmail.com>
2023-02-19 22:41:36 +0700

    update

--- app/page.tsx
+++ app/page.tsx
@@ -22,14 +22,14 @@ const fetcher = async () => {
 const SkeletonItem = () => {
   return (
     <div className="items flex gap-[10px]">
-      <div className="h-full w-[15%] bg-slate-200 animate-pulse rounded-mdr"></div>
+      <div className="h-full w-[15%] bg-slate-200 animate-pulse rounded-md"></div>
       <div className="h-full w-[60%] bg-slate-200 animate-pulse rounded-md"></div>
       <div className="h-full w-[25%] bg-slate-200 animate-pulse rounded-md"></div>
     </div>
   );
 };
 const Skeleton = () => {
-  const items = Array.from({ length: 114 }, (_, i) => <SkeletonItem key={i} />);
+  const items = Array.from({ length: 15 }, (_, i) => <SkeletonItem key={i} />);
   return <main className="mainBar">{items}</main>;
 };
 
